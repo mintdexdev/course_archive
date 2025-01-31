4a293fede8a7bad848bc733df56671f2e956ec95
mintdexdev <dexdevpersona@gmail.com>
2025-01-31 11:43:21 +0530

    remove:16, rename and add:16 115-118 -> 16 115-121

---
+++ 16 115-121/script1.js
@@ -0,0 +1,10 @@
+document.querySelector("h1").style.color = "coral";
+document.querySelector("button").style.borderRadius = "10px";
+document.querySelector("button").classList.add("huge");
+// document.querySelector("button").style.backgroundColor = "mistyrose";
+
+document.querySelector("h1").innerHTML = "<u>change html</u>";
+document.querySelector("h1").textContent = "<u>only change text</u>";
+
+// document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
--- 16-115-118/script1.js
+++
@@ -1,3 +0,0 @@
-document.querySelector("h1").style.color = "coral";
-document.querySelector("button").style.borderRadius = "10px";
-document.querySelector("button").style.backgroundColor = "mistyrose";
